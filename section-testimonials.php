cff27e008008906fbe5c6056705349f5441a2c6c
Danielle <dnorris@sovenco.com>
2017-06-16 16:13:07 -0600

    Update section-testimonials.php

--- section-testimonials.php
+++ section-testimonials.php
@@ -1,27 +1,27 @@
 <?php
-$onepress_testimonial_id       = get_theme_mod( 'onepress_testimonial_id', esc_html__('testimonials', 'onepress-plus') );
-$onepress_testimonials_disable = get_theme_mod( 'onepress_testimonials_disable' ) ==  1 ? true : false;
-$onepress_testimonial_title    = get_theme_mod( 'onepress_testimonial_title', esc_html__('Testimonials', 'onepress-plus' ));
-$onepress_testimonial_subtitle = get_theme_mod( 'onepress_testimonial_subtitle', esc_html__('Section subtitle', 'onepress-plus' ));
-$desc = get_theme_mod( 'onepress_testimonial_desc' );
+$sovenco_testimonial_id       = get_theme_mod( 'sovenco_testimonial_id', esc_html__('testimonials', 'sovenco-plus') );
+$sovenco_testimonials_disable = get_theme_mod( 'sovenco_testimonials_disable' ) ==  1 ? true : false;
+$sovenco_testimonial_title    = get_theme_mod( 'sovenco_testimonial_title', esc_html__('Testimonials', 'sovenco-plus' ));
+$sovenco_testimonial_subtitle = get_theme_mod( 'sovenco_testimonial_subtitle', esc_html__('Section subtitle', 'sovenco-plus' ));
+$desc = get_theme_mod( 'sovenco_testimonial_desc' );
 ?>
-<?php if ( ! $onepress_testimonials_disable  ) : ?>
-<?php if ( ! onepress_is_selective_refresh() ){ ?>
-    <section id="<?php if ( $onepress_testimonial_id != '' ) echo $onepress_testimonial_id; ?>" <?php do_action( 'onepress_section_atts', 'testimonials' ); ?> class="<?php echo esc_attr( apply_filters( 'onepress_section_class', 'section-testimonials onepage-section section-meta section-padding section-padding-lg', 'testimonials' ) ); ?>">
+<?php if ( ! $sovenco_testimonials_disable  ) : ?>
+<?php if ( ! sovenco_is_selective_refresh() ){ ?>
+    <section id="<?php if ( $sovenco_testimonial_id != '' ) echo $sovenco_testimonial_id; ?>" <?php do_action( 'sovenco_section_atts', 'testimonials' ); ?> class="<?php echo esc_attr( apply_filters( 'sovenco_section_class', 'section-testimonials onepage-section section-meta section-padding section-padding-lg', 'testimonials' ) ); ?>">
 <?php } ?>
-        <?php do_action( 'onepress_section_before_inner', 'testimonials' ); ?>
+        <?php do_action( 'sovenco_section_before_inner', 'testimonials' ); ?>
         <div class="container">
-            <?php if ( $onepress_testimonial_title || $onepress_testimonial_subtitle || $desc ) { ?>
+            <?php if ( $sovenco_testimonial_title || $sovenco_testimonial_subtitle || $desc ) { ?>
                 <div class="section-title-area">
-                    <?php if ($onepress_testimonial_subtitle != '') echo '<h5 class="section-subtitle">' . esc_html($onepress_testimonial_subtitle) . '</h5>'; ?>
-                    <?php if ($onepress_testimonial_title != '') echo '<h2 class="section-title">' . esc_html($onepress_testimonial_title) . '</h2>'; ?>
+                    <?php if ($sovenco_testimonial_subtitle != '') echo '<h5 class="section-subtitle">' . esc_html($sovenco_testimonial_subtitle) . '</h5>'; ?>
+                    <?php if ($sovenco_testimonial_title != '') echo '<h2 class="section-title">' . esc_html($sovenco_testimonial_title) . '</h2>'; ?>
                     <?php if ($desc) {
-                        echo '<div class="section-desc">' . apply_filters( 'onepress_the_content', wp_kses_post( $desc ) ) . '</div>';
+                        echo '<div class="section-desc">' . apply_filters( 'sovenco_the_content', wp_kses_post( $desc ) ) . '</div>';
                     } ?>
                 </div>
                 <?php
             }
-            $testimonials = get_theme_mod( 'onepress_testimonial_boxes', '' );
+            $testimonials = get_theme_mod( 'sovenco_testimonial_boxes', '' );
 
             if ( is_string( $testimonials ) ) {
                 $testimonials = json_decode( $testimonials, true );
@@ -30,39 +30,39 @@ $desc = get_theme_mod( 'onepress_testimonial_desc' );
             if ( ! is_array( $testimonials ) || empty( $testimonials ) ) {
                 $testimonials = array(
                     array(
-                        'title' 		=> esc_html__( 'Praesent placerat', 'onepress-plus' ),
-                        'name' 			=> esc_html__( 'Alexander Rios', 'onepress-plus' ),
-                        'subtitle' 		=> esc_html__( 'Founder & CEO', 'onepress-plus' ),
+                        'title' 		=> esc_html__( 'Praesent placerat', 'sovenco-plus' ),
+                        'name' 			=> esc_html__( 'Alexander Rios', 'sovenco-plus' ),
+                        'subtitle' 		=> esc_html__( 'Founder & CEO', 'sovenco-plus' ),
                         'style'         => 'warning',
                         'image' 		=> array(
                             'url' => get_template_directory_uri() . '/assets/images/testimonial_1.jpg',
                             'id'  => ''
                         ),
-                        'content' 		=> esc_html__( 'Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.', 'onepress-plus' ),
+                        'content' 		=> esc_html__( 'Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.', 'sovenco-plus' ),
 
                     ),
                     array(
-                        'title' 		=> esc_html__( 'Cras iaculis', 'onepress-plus' ),
-                        'name' 			=> esc_html__( 'Alexander Max', 'onepress-plus' ),
-                        'subtitle' 		=> esc_html__( 'Founder & CEO', 'onepress-plus' ),
+                        'title' 		=> esc_html__( 'Cras iaculis', 'sovenco-plus' ),
+                        'name' 			=> esc_html__( 'Alexander Max', 'sovenco-plus' ),
+                        'subtitle' 		=> esc_html__( 'Founder & CEO', 'sovenco-plus' ),
                         'style'         => 'success',
                         'image' 		=> array(
                             'url' => get_template_directory_uri() . '/assets/images/testimonial_2.jpg',
                             'id'  => ''
                         ),
-                        'content' 		=> esc_html__( 'Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue eu vulputate.', 'onepress-plus' ),
+                        'content' 		=> esc_html__( 'Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue eu vulputate.', 'sovenco-plus' ),
 
                     ),
                     array(
-                        'title' 		=> esc_html__( 'Fusce lobortis', 'onepress-plus' ),
-                        'name' 			=> esc_html__( 'Peter Mendez', 'onepress-plus' ),
-                        'subtitle' 		=> esc_html__( 'Example Company', 'onepress-plus' ),
+                        'title' 		=> esc_html__( 'Fusce lobortis', 'sovenco-plus' ),
+                        'name' 			=> esc_html__( 'Peter Mendez', 'sovenco-plus' ),
+                        'subtitle' 		=> esc_html__( 'Example Company', 'sovenco-plus' ),
                         'style'         => 'theme-primary',
                         'image' 		=> array(
                             'url' => get_template_directory_uri() . '/assets/images/testimonial_3.jpg',
                             'id'  => ''
                         ),
-                        'content' 		=> esc_html__( 'Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, euismod vel, velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper magna egestas sem.', 'onepress-plus' ),
+                        'content' 		=> esc_html__( 'Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, euismod vel, velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper magna egestas sem.', 'sovenco-plus' ),
 
                     ),
                 );
@@ -147,8 +147,8 @@ $desc = get_theme_mod( 'onepress_testimonial_desc' );
                 ?>
             </div>
         </div>
-        <?php do_action( 'onepress_section_after_inner', 'testimonials' ); ?>
-<?php if ( ! onepress_is_selective_refresh() ){ ?>
+        <?php do_action( 'sovenco_section_after_inner', 'testimonials' ); ?>
+<?php if ( ! sovenco_is_selective_refresh() ){ ?>
     </section>
 <?php } ?>
 <?php endif; ?>
