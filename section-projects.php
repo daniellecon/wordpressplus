610cd80adf1429c6c805a6e70660ea8643c8ceb1
Danielle <dnorris@sovenco.com>
2017-06-16 16:15:08 -0600

    Update section-projects.php

--- section-projects.php
+++ section-projects.php
@@ -1,21 +1,21 @@
 <?php
-$onepress_projects_id       = get_theme_mod( 'onepress_projects_id', 'projects' );
-$onepress_projects_title    = get_theme_mod( 'onepress_projects_title', __('Highlight Projects', 'onepress-plus' ));
-$onepress_projects_subtitle = get_theme_mod( 'onepress_projects_subtitle', __('Some of our works', 'onepress-plus' ));
-$is_ajax = get_theme_mod( 'onepress_project_ajax', 1 );
-$desc = get_theme_mod( 'onepress_projects_desc' );
+$sovenco_projects_id       = get_theme_mod( 'sovenco_projects_id', 'projects' );
+$sovenco_projects_title    = get_theme_mod( 'sovenco_projects_title', __('Highlight Projects', 'sovenco-plus' ));
+$sovenco_projects_subtitle = get_theme_mod( 'sovenco_projects_subtitle', __('Some of our works', 'sovenco-plus' ));
+$is_ajax = get_theme_mod( 'sovenco_project_ajax', 1 );
+$desc = get_theme_mod( 'sovenco_projects_desc' );
 
 ?>
-<?php if ( ! onepress_is_selective_refresh() ){ ?>
-<section <?php if ( $onepress_projects_id ) { ?>id="<?php echo esc_attr( $onepress_projects_id ); ?>" <?php } ?> class="<?php echo esc_attr( apply_filters( 'onepress_section_class', 'section-padding section-projects onepage-section', 'projects' ) ); ?>">
+<?php if ( ! sovenco_is_selective_refresh() ){ ?>
+<section <?php if ( $sovenco_projects_id ) { ?>id="<?php echo esc_attr( $sovenco_projects_id ); ?>" <?php } ?> class="<?php echo esc_attr( apply_filters( 'sovenco_section_class', 'section-padding section-projects onepage-section', 'projects' ) ); ?>">
 <?php } ?>
     <div class="container">
-        <?php if ( $onepress_projects_title || $onepress_projects_subtitle || $desc ) { ?>
+        <?php if ( $sovenco_projects_title || $sovenco_projects_subtitle || $desc ) { ?>
         <div class="section-title-area">
-            <?php if ( $onepress_projects_subtitle != '' ) echo '<h5 class="section-subtitle">' . esc_html( $onepress_projects_subtitle ) . '</h5>'; ?>
-            <?php if ( $onepress_projects_title != '' ) echo '<h2 class="section-title">' . esc_html( $onepress_projects_title ) . '</h2>'; ?>
+            <?php if ( $sovenco_projects_subtitle != '' ) echo '<h5 class="section-subtitle">' . esc_html( $sovenco_projects_subtitle ) . '</h5>'; ?>
+            <?php if ( $sovenco_projects_title != '' ) echo '<h2 class="section-title">' . esc_html( $sovenco_projects_title ) . '</h2>'; ?>
             <?php if ( $desc ) {
-                echo '<div class="section-desc">' . apply_filters( 'onepress_the_content', wp_kses_post( $desc ) )  . '</div>';
+                echo '<div class="section-desc">' . apply_filters( 'sovenco_the_content', wp_kses_post( $desc ) )  . '</div>';
             } ?>
         </div>
         <?php } ?>
@@ -24,9 +24,9 @@ $desc = get_theme_mod( 'onepress_projects_desc' );
             $args = array(
                 'post_type' => 'portfolio',
                 'post_status' => 'publish',
-                'posts_per_page' => get_theme_mod( 'onepress_projects_number', 6 ),
-                'order' =>  get_theme_mod( 'onepress_projects_order', 'DESC' ),
-                'orderby' => get_theme_mod( 'onepress_projects_orderby', 'ID' ),
+                'posts_per_page' => get_theme_mod( 'sovenco_projects_number', 6 ),
+                'order' =>  get_theme_mod( 'sovenco_projects_order', 'DESC' ),
+                'orderby' => get_theme_mod( 'sovenco_projects_orderby', 'ID' ),
                 'suppress_filters' => 0,
             );
 
@@ -47,7 +47,7 @@ $desc = get_theme_mod( 'onepress_projects_desc' );
                                     echo '<a href="'.get_permalink( $post->ID ).'">';
                                 }
                                 if ( has_post_thumbnail() ) {
-                                    the_post_thumbnail( 'onepress-medium' );
+                                    the_post_thumbnail( 'sovenco-medium' );
                                 }
                                 if ( ! $is_ajax ) {
                                     echo '</a>';
@@ -85,6 +85,6 @@ $desc = get_theme_mod( 'onepress_projects_desc' );
             <div class="clear"></div>
         </div>
     </div>
-<?php if ( ! onepress_is_selective_refresh() ){ ?>
+<?php if ( ! sovenco_is_selective_refresh() ){ ?>
 </section>
 <?php } ?>
