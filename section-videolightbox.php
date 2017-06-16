10a313c2cfe7bdb427456f65f438db46eb9bfea2
Danielle <dnorris@sovenco.com>
2017-06-16 16:09:50 -0600

    Update section-videolightbox.php

--- section-videolightbox.php
+++ section-videolightbox.php
@@ -1,16 +1,16 @@
 <?php
-$id       = get_theme_mod( 'onepress_videolightbox_id', 'videolightbox' );
-$disable  = get_theme_mod( 'onepress_videolightbox_disable' ) == 1 ? true : false;
-$heading  = get_theme_mod( 'onepress_videolightbox_title' );
-$video    = get_theme_mod( 'onepress_videolightbox_url' );
+$id       = get_theme_mod( 'sovenco_videolightbox_id', 'videolightbox' );
+$disable  = get_theme_mod( 'sovenco_videolightbox_disable' ) == 1 ? true : false;
+$heading  = get_theme_mod( 'sovenco_videolightbox_title' );
+$video    = get_theme_mod( 'sovenco_videolightbox_url' );
 
 if ( ! $disable && ( $video || $heading ) ) {
-    $image    = get_theme_mod( 'onepress_videolightbox_image' );
+    $image    = get_theme_mod( 'sovenco_videolightbox_image' );
     ?>
-    <?php if ( ! onepress_is_selective_refresh() ){ ?>
-    <section id="<?php if ($id != '') echo esc_attr( $id ); ?>" <?php do_action('onepress_section_atts', 'videolightbox'); ?> class="<?php echo esc_attr(apply_filters('onepress_section_class', 'section-videolightbox section-padding section-padding-larger section-inverse onepage-section', 'videolightbox')); ?>">
+    <?php if ( ! sovenco_is_selective_refresh() ){ ?>
+    <section id="<?php if ($id != '') echo esc_attr( $id ); ?>" <?php do_action('sovenco_section_atts', 'videolightbox'); ?> class="<?php echo esc_attr(apply_filters('sovenco_section_class', 'section-videolightbox section-padding section-padding-larger section-inverse onepage-section', 'videolightbox')); ?>">
     <?php } ?>
-        <?php do_action('onepress_section_before_inner', 'videolightbox'); ?>
+        <?php do_action('sovenco_section_before_inner', 'videolightbox'); ?>
         <div class="container">
             <?php if ( $video ) { ?>
                 <div class="videolightbox__icon videolightbox-popup">
@@ -23,8 +23,8 @@ if ( ! $disable && ( $video || $heading ) ) {
                 <h2 class="videolightbox__heading"><?php echo do_shortcode( wp_kses_post( $heading ) ); ?></h2>
             <?php } ?>
         </div>
-        <?php do_action('onepress_section_after_inner', 'videolightbox'); ?>
-    <?php if ( ! onepress_is_selective_refresh() ){ ?>
+        <?php do_action('sovenco_section_after_inner', 'videolightbox'); ?>
+    <?php if ( ! sovenco_is_selective_refresh() ){ ?>
     </section>
     <?php } ?>
 <?php  }
