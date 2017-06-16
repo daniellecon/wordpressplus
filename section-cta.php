a805b06e5e41280d676abb26427de6ff4fe7a471
Danielle <dnorris@sovenco.com>
2017-06-16 16:20:34 -0600

    Update section-cta.php

--- section-cta.php
+++ section-cta.php
@@ -1,23 +1,23 @@
 <?php
-$onepress_cta_id       = get_theme_mod( 'onepress_cta_id', 'section-cta' );
-$onepress_cta_title    = get_theme_mod( 'onepress_cta_title', __('Use these ribbons to display calls to action mid-page.', 'onepress-plus' ));
-$button_label = get_theme_mod( 'onepress_cta_btn_label', __('Button text', 'onepress-plus' ));
-$button_link = get_theme_mod( 'onepress_cta_btn_link', '#' );
+$sovenco_cta_id       = get_theme_mod( 'sovenco_cta_id', 'section-cta' );
+$sovenco_cta_title    = get_theme_mod( 'sovenco_cta_title', __('Use these ribbons to display calls to action mid-page.', 'sovenco-plus' ));
+$button_label = get_theme_mod( 'sovenco_cta_btn_label', __('Button text', 'sovenco-plus' ));
+$button_link = get_theme_mod( 'sovenco_cta_btn_link', '#' );
 
 ?>
-<?php if ( ! onepress_is_selective_refresh() ){ ?>
-<section <?php if ( $onepress_cta_id ) { ?>id="<?php echo esc_attr( $onepress_cta_id ); ?>" <?php } ?> class="<?php echo esc_attr( apply_filters( 'onepress_section_class', 'section-cta section-padding section-inverse onepage-section', 'cta' ) ); ?>">
+<?php if ( ! sovenco_is_selective_refresh() ){ ?>
+<section <?php if ( $sovenco_cta_id ) { ?>id="<?php echo esc_attr( $sovenco_cta_id ); ?>" <?php } ?> class="<?php echo esc_attr( apply_filters( 'sovenco_section_class', 'section-cta section-padding section-inverse onepage-section', 'cta' ) ); ?>">
 <?php } ?>
-    <?php do_action( 'onepress_section_before_inner', 'cta' ); ?>
+    <?php do_action( 'sovenco_section_before_inner', 'cta' ); ?>
 
     <div class="container">
         <div class="row">
             <div class="col-md-12 col-lg-9 cta-heading">
-                <h2><?php echo wp_kses_post( $onepress_cta_title ); ?></h2>
+                <h2><?php echo wp_kses_post( $sovenco_cta_title ); ?></h2>
             </div>
             <div class="col-md-12 col-lg-3 cta-button-area">
                 <?php if ( $button_label ) {
-                    $style = sanitize_text_field( get_theme_mod( 'onepress_cta_btn_link_style', 'theme-primary' ) );
+                    $style = sanitize_text_field( get_theme_mod( 'sovenco_cta_btn_link_style', 'theme-primary' ) );
                     $btn_class = '';
                     if ( !$style || $style =='theme-primary' || strpos( $style, 'btn-' ) !== 0 ) {
                         $btn_class = 'btn-theme-primary-outline';
@@ -32,7 +32,7 @@ $button_link = get_theme_mod( 'onepress_cta_btn_link', '#' );
         </div>
     </div>
 
-    <?php do_action( 'onepress_section_after_inner', 'cta' ); ?>
-<?php if ( ! onepress_is_selective_refresh() ){ ?>
+    <?php do_action( 'sovenco_section_after_inner', 'cta' ); ?>
+<?php if ( ! sovenco_is_selective_refresh() ){ ?>
 </section>
 <?php } ?>
