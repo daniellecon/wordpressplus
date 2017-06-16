f4d1dfbf925192be1e4bf989f5120d12abc189fb
Danielle <dnorris@sovenco.com>
2017-06-16 17:02:51 -0600

    Update auto-apply.php

--- auto-apply.php
+++ auto-apply.php
@@ -4,10 +4,10 @@
  *
  * Auto add style for typography settings
  *
- * @see onepress_typography_helper_auto_apply
+ * @see sovenco_typography_helper_auto_apply
  */
-onepress_typography_helper_auto_apply(
-    'onepress_typo_p', // customize setting ID
+sovenco_typography_helper_auto_apply(
+    'sovenco_typo_p', // customize setting ID
     'body, body p' // CSS selector
     /*
     array( // default value
@@ -24,24 +24,24 @@ onepress_typography_helper_auto_apply(
     */
 );
 
-onepress_typography_helper_auto_apply(
-    'onepress_typo_site_title', // customize setting ID
+sovenco_typography_helper_auto_apply(
+    'sovenco_typo_site_title', // customize setting ID
     '#page .site-branding .site-title, #page .site-branding .site-text-logo' // CSS selector
 );
 
-onepress_typography_helper_auto_apply(
-    'onepress_typo_site_tagline', // customize setting ID
+sovenco_typography_helper_auto_apply(
+    'sovenco_typo_site_tagline', // customize setting ID
     '#page .site-branding .site-description' // CSS selector
 );
 
 
-onepress_typography_helper_auto_apply(
-    'onepress_typo_menu', // customize setting ID
-    '.onepress-menu a' // CSS selector
+sovenco_typography_helper_auto_apply(
+    'sovenco_typo_menu', // customize setting ID
+    '.sovenco-menu a' // CSS selector
 );
 
-onepress_typography_helper_auto_apply(
-    'onepress_typo_heading', // customize setting ID
+sovenco_typography_helper_auto_apply(
+    'sovenco_typo_heading', // customize setting ID
     'body h1, body h2, body h3, body h4, body h5, body h6,
     body .section-title-area .section-title, body .section-title-area .section-subtitle, body .hero-content-style1 h2' // CSS selector
 );
