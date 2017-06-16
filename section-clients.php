54bfd78fef0e9b38d7033ca1a43289636a8b2cc8
Danielle <dnorris@sovenco.com>
2017-06-16 16:21:42 -0600

    Update section-clients.php

--- section-clients.php
+++ section-clients.php
@@ -1,12 +1,12 @@
 <?php
-$id       = get_theme_mod( 'onepress_clients_id', 'clients' );
-$title    = get_theme_mod( 'onepress_clients_title');
-$subtitle = get_theme_mod( 'onepress_clients_subtitle', __('Have been featured on', 'onepress-plus' ));
-$desc     = get_theme_mod( 'onepress_clients_desc' );
+$id       = get_theme_mod( 'sovenco_clients_id', 'clients' );
+$title    = get_theme_mod( 'sovenco_clients_title');
+$subtitle = get_theme_mod( 'sovenco_clients_subtitle', __('Have been featured on', 'sovenco-plus' ));
+$desc     = get_theme_mod( 'sovenco_clients_desc' );
 
 ?>
-<?php if ( ! onepress_is_selective_refresh() ){ ?>
-<section <?php if ( $id ) { ?>id="<?php echo esc_attr( $id ); ?>" <?php } ?> class="<?php echo esc_attr( apply_filters( 'onepress_section_class', 'section-padding section-clients onepage-section', 'clients' ) ); ?>">
+<?php if ( ! sovenco_is_selective_refresh() ){ ?>
+<section <?php if ( $id ) { ?>id="<?php echo esc_attr( $id ); ?>" <?php } ?> class="<?php echo esc_attr( apply_filters( 'sovenco_section_class', 'section-padding section-clients onepage-section', 'clients' ) ); ?>">
 <?php } ?>
     <div class="container">
         <?php if ( $title || $subtitle || $desc ) { ?>
@@ -14,14 +14,14 @@ $desc     = get_theme_mod( 'onepress_clients_desc' );
                 <?php if ($subtitle != '') echo '<h5 class="section-subtitle">' . esc_html($subtitle) . '</h5>'; ?>
                 <?php if ($title != '') echo '<h2 class="section-title">' . esc_html($title) . '</h2>'; ?>
                 <?php if ($desc ) {
-                    echo '<div class="section-desc">' . apply_filters( 'onepress_the_content', wp_kses_post( $desc ) ) . '</div>';
+                    echo '<div class="section-desc">' . apply_filters( 'sovenco_the_content', wp_kses_post( $desc ) ) . '</div>';
                 } ?>
             </div>
             <?php
         }
 
-        $columns = get_theme_mod( 'onepress_clients_layout', 5 );
-        $clients = get_theme_mod( 'onepress_clients' );
+        $columns = get_theme_mod( 'sovenco_clients_layout', 5 );
+        $clients = get_theme_mod( 'sovenco_clients' );
         if ( is_string( $clients ) ) {
             $clients = json_decode( $clients , true );
         }
@@ -29,42 +29,42 @@ $desc     = get_theme_mod( 'onepress_clients_desc' );
         if ( empty( $clients ) ){
             $clients = array(
                 array(
-                    'title' => esc_html__( 'Hostingco', 'onepress-plus' ),
+                    'title' => esc_html__( 'Hostingco', 'sovenco-plus' ),
                     'image'  => array(
                         'id'=> '',
-                        'url'=> ONEPRESS_PLUS_URL.'assets/images/client_logo_1.png',
+                        'url'=> sovenco_PLUS_URL.'assets/images/client_logo_1.png',
                     ),
                     'link' => ''
                 ),
                 array(
-                    'title' => esc_html__( 'Religion', 'onepress-plus' ),
+                    'title' => esc_html__( 'Religion', 'sovenco-plus' ),
                     'image'  => array(
                         'id'=> '',
-                        'url'=> ONEPRESS_PLUS_URL.'assets/images/client_logo_2.png',
+                        'url'=> sovenco_PLUS_URL.'assets/images/client_logo_2.png',
                     ),
                     'link' => ''
                 ),
                 array(
-                    'title' => esc_html__( 'Viento', 'onepress-plus' ),
+                    'title' => esc_html__( 'Viento', 'sovenco-plus' ),
                     'image'  => array(
                         'id'=> '',
-                        'url'=> ONEPRESS_PLUS_URL.'assets/images/client_logo_3.png',
+                        'url'=> sovenco_PLUS_URL.'assets/images/client_logo_3.png',
                     ),
                     'link' => ''
                 ),
                 array(
-                    'title' => esc_html__( 'Naturefirst', 'onepress-plus' ),
+                    'title' => esc_html__( 'Naturefirst', 'sovenco-plus' ),
                     'image'  => array(
                         'id'=> '',
-                        'url'=> ONEPRESS_PLUS_URL.'assets/images/client_logo_4.png',
+                        'url'=> sovenco_PLUS_URL.'assets/images/client_logo_4.png',
                     ),
                     'link' => ''
                 ),
                 array(
-                    'title' => esc_html__( 'Imagine', 'onepress-plus' ),
+                    'title' => esc_html__( 'Imagine', 'sovenco-plus' ),
                     'image'  => array(
                         'id'=> '',
-                        'url'=> ONEPRESS_PLUS_URL.'assets/images/client_logo_5.png',
+                        'url'=> sovenco_PLUS_URL.'assets/images/client_logo_5.png',
                     ),
                     'link' => ''
                 ),
@@ -76,7 +76,7 @@ $desc     = get_theme_mod( 'onepress_clients_desc' );
             <?php
             $j = 0;
             foreach ( $clients as $client ) {
-                $url =  OnePress_PLus::get_media_url( $client['image'] );
+                $url =  sovenco_PLus::get_media_url( $client['image'] );
                 $classes = '';
                 if ( $url ) {
                     if ($j >= $columns) {
@@ -102,6 +102,6 @@ $desc     = get_theme_mod( 'onepress_clients_desc' );
         </div>
         <?php } ?>
     </div>
-<?php if ( ! onepress_is_selective_refresh() ){ ?>
+<?php if ( ! sovenco_is_selective_refresh() ){ ?>
 </section>
 <?php } ?>
